6ecd45bc6253ed189f2092bc91c08bd53061a7c5
Marcelo3Pereyra <vecopereyra@gmail.com>
2020-11-25 23:32:23 -0300

    Creacion de verificardor de token y ADMIN_ROLE

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,16 +1,16 @@
-const { response } = require('express');
-const bcrypt = require('bcrypt');
 const _ = require('underscore');
+const bcrypt = require('bcrypt');
 const express = require('express');
 
 
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion'); //Solo importa la funcion con destructuracion
 
 const app = express();
 
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -25,13 +25,13 @@ app.get('/usuario', function(req, res) {
         .exec((err, usuarios) => { //ejecuta el query  
 
 
+
             if (err) {
                 res.status(400).json({
                     ok: false,
                     mensaje: err
                 })
             }
-
             Usuario.count({ estado: true }, (err, conteo) => {
                 res.json({
                     ok: true,
@@ -39,14 +39,10 @@ app.get('/usuario', function(req, res) {
                     cuantos: conteo
                 })
             })
-
-
-
         })
-
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
 
     let body = req.body;
@@ -81,7 +77,7 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -105,7 +101,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
 
--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
+
 const bodyParser = require('body-parser');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,8 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+//configuracion global de rutas
+app.use(require('./routes/index'));
 
-app.use(require('./routes/usuario'));
 
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
